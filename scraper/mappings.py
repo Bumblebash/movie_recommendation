b3d3bdc7648608138dcfc8f9374d2e6440fa191e
BASH7 <94570601+Bumblebash@users.noreply.github.com>
2025-02-28 19:48:32 +0300

    Add files via upload

---
+++ scraper/mappings.py
@@ -0,0 +1,18 @@
+URLS = {
+    "Drama": 'https://www.imdb.com/search/title/?title_type=feature&genres=drama',
+    "Action": 'https://www.imdb.com/search/title/?title_type=feature&genres=action',
+    "Comedy": 'https://www.imdb.com/search/title/?title_type=feature&genres=comedy',
+    "Horror": 'https://www.imdb.com/search/title/?title_type=feature&genres=horror',
+    "Crime": 'https://www.imdb.com/search/title/?title_type=feature&genres=crime',
+}
+
+EMOTION_TO_GENRE = {
+    "Sad": "Drama",
+    "Disgust": "Musical",
+    "Anger": "Family",
+    "Anticipation": "Thriller",
+    "Fear": "Sport",
+    "Enjoyment": "Thriller",
+    "Trust": "Western",
+    "Surprise": "Film-Noir"
+}
